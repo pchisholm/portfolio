68bc73c7ff85a0b0b7604db2757667d34e414f97
pchisholm <chisholm@ca.ibm.com>
2017-03-30 09:46:56 -0300

    ui updates to admin card padding and fab toolbar

--- server/api/link/link.controller.js
+++ server/api/link/link.controller.js
@@ -114,6 +114,7 @@ export function upsert(req, res) {
   if(req.body._id) {
     delete req.body._id;
   }
+
   return Link.findOneAndUpdate({_id: req.params.id}, req.body, {upsert: true, setDefaultsOnInsert: true, runValidators: true}).exec()
     .then(respondWithResult(res))
     .catch(handleError(res));
